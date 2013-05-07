940ede9059c92991d589f4032de9c96362d89982
Harley Inbody <hinbody@gmail.com>
2013-05-06 21:58:08 -0400

    start a bmx racing info script to explore argument variables as extra credit

---
+++ first_bmx_script.rb
@@ -0,0 +1,6 @@
+# take first name, last name, current age, and class proficiency as cli input
+# and assign them to the appropriate variables
+fname, lname, age, proficiency = ARGV 
+
+puts "Your name is: #{fname} #{lname}"
+puts "And you're racing as #{age} #{proficiency}"
